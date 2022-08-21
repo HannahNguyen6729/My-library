db36b5372c2093418c67bb5541c301935b58ef00
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2022-08-21 21:44:05 +0300

    set up project

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const expressLayout = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const router = require("./routes/routes");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayout);
+app.use(express.static("public"));
+
+//connect to database
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("connected to the database"));
+
+//routes
+app.use("/", router);
+
+//server is running
+app.listen(process.env.PORT || 3000, () => console.log("listening on port"));
